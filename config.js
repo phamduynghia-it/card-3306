f315a28062cf915172e78c9caa0b68d311a76327
phamduynghia-it <162310524+phamduynghia-it@users.noreply.github.com>
2026-09-17 14:59:11 +0700

    Deploy greeting card with dynamic assets and custom workflow

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    // Tiêu đề của trang và thiệp
+    title: `Yêu Em 💖`,
+
+    // Link nhạc nền
+    music: "./assets/user_music.mp3",
+    
+    // Lời nhắn hiển thị trong phần thiệp
+    popupMessage: `💖 Gửi đến em người anh thương nhất! 💝 Cảm ơn em đã đến bên anh, cùng anh đi qua những ngày tháng đẹp nhất. Anh yêu em thật nhiều, mãi mãi bên nhau em nhé! 💕💗💞`,
+    
+    // Các câu chữ bay lên xung quanh trái tim 3D
+    messages: ["Ck yêu xù ❤️","Xù ❤️","Mãi bên nhau","Anh yêu em 💗💞","1 Năm Bên Nhau"],
+    
+    // Các hình ảnh bay lên khi mở hộp quà (có thể thêm nhiều ảnh)
+    images: [],
+    
+    // Màu của trái tim 3D (mã màu Hex)
+    heartColor: "#fd9595"
+};
